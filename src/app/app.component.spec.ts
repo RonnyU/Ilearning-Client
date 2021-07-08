4efc3bf9857706e7eadd641adcb22fe27aaf51dc
RonnyU <ronnymagdiel@gmail.com>
2021-07-07 21:29:23 -0600

    adding build folder to the repo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ccc-client'`, () => {
+  it(`should have as title 'ilearning-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ccc-client');
+    expect(app.title).toEqual('ilearning-client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ccc-client app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'ilearning-client app is running!'
+    );
   });
 });
